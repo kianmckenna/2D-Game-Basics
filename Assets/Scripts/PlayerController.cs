4602b7f37883bda039b3f87780148b203225cea0
kianmckenna <kgmckenna04@gmail.com>
2024-11-01 16:37:41 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+// using System.Numerics;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        transform.position = position;
+    }
+}
